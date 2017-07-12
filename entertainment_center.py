47ff388aaef3cf53b7af009fefaa7322a890e387
jeevanga <jeevan.george.abraham@gmail.com>
2017-07-12 06:38:21 +0100

    All included
    Jquery script now includes all code under the click function - code targeting trailer video and storyline writeup.

--- entertainment_center.py
+++ entertainment_center.py
@@ -11,14 +11,9 @@
 import media
 import fresh_tomatoes
 
-#print menace_ii_society.storyline
-#menace_ii_society.show_poster()
-#menace_ii_society.show_trailer()
-#avatar = media.Movie()
+new_jack_city = media.Movie("New Jack City", "A crime lord ascends to power and becomes megalomaniacal while a maverick police detective vows to stop him.","Drug tycoon Nino Brown (Wesley Snipes) and his minions, known collectively as the Cash Money Brothers, have rapidly risen to the top of the New York City narcotics trade. Under Nino's heartless leadership, the drug operation has grown into a multimillion-dollar empire. Scotty (Ice-T) and Nick (Judd Nelson), two police officers who know their way around the streets of Harlem, aim to bring Nino and his cohorts down. To do so, though, they'll have to play by Nino's rules and go undercover.", "https://upload.wikimedia.org/wikipedia/en/e/e4/New_jack_city.jpg", "https://www.youtube.com/watch?v=CXrTXaKr0x4")
 
 boyz_n_the_hood = media.Movie("Boyz N The Hood", "Follows the lives of three young males living in the Crenshaw ghetto of Los Angeles, dissecting questions of race, relationships, violence and future prospects.", "Tre (Cuba Gooding Jr.) is sent to live with his father, Furious Styles (Larry Fishburne), in tough South Central Los Angeles. Although his hard-nosed father instills proper values and respect in him, and his devout girlfriend Brandi (Nia Long) teaches him about faith, Tre's friends Doughboy (Ice Cube) and Ricky (Morris Chestnut) don't have the same kind of support and are drawn into the neighborhood's booming drug and gang culture, with increasingly tragic results.", "https://upload.wikimedia.org/wikipedia/en/c/c3/Boyz_n_the_hood_poster.jpg", "https://www.youtube.com/watch?v=AE4BdijGtu4")
-
-new_jack_city = media.Movie("New Jack City", "A crime lord ascends to power and becomes megalomaniacal while a maverick police detective vows to stop him.","Drug tycoon Nino Brown (Wesley Snipes) and his minions, known collectively as the Cash Money Brothers, have rapidly risen to the top of the New York City narcotics trade. Under Nino's heartless leadership, the drug operation has grown into a multimillion-dollar empire. Scotty (Ice-T) and Nick (Judd Nelson), two police officers who know their way around the streets of Harlem, aim to bring Nino and his cohorts down. To do so, though, they'll have to play by Nino's rules and go undercover.", "https://upload.wikimedia.org/wikipedia/en/e/e4/New_jack_city.jpg", "https://www.youtube.com/watch?v=CXrTXaKr0x4")
 
 menace_ii_society = media.Movie("Menace II Society", "A young street hustler attempts to escape the rigors and temptations of the ghetto in a quest for a better life.","After growing up in the gang lifestyle of the Los Angeles projects, 18-year-old Caine Lawson (Tyrin Turner) wants a way out. Everyone around him, including his unpredictable friend O-Dog (Larenz Tate), is trapped in their lives of crime and violence. With the help of his caring teacher (Charles Dutton) and supportive girlfriend (Jada Pinkett), Caine plans to leave the city for good. But in a series of tragic events, Caine realizes that escape will not be easy.", "https://image.tmdb.org/t/p/original/lstD3oZoNReyKdx6PXfTiBsUkKQ.jpg", "https://www.youtube.com/watch?v=XrMCRkTRmiY")
 
@@ -34,5 +29,5 @@ poetic_justice = media.Movie("Poetic Justice","In this film, we see the world th
 
 booty_call = media.Movie("Booty Call","Two friends who have gone too long without sex set out to get some." ,"Rushon (Tommy Davidson) is sexually pent-up and ready to take thing things to the next level with his girlfriend, Nikki (Tamala Jones). But when he calls for a date, she asks to make it a double -- bringing along her brash friend Lysterine (Vivica A. Fox), whom Rushon sets up with his lewd buddy, Bunz (Jamie Foxx). Things go better than expected. As the evening transitions from the restaurant to the bedroom, the two men go on a madcap search for what will surely make the night complete: condoms.", "https://upload.wikimedia.org/wikipedia/en/d/d7/Booty_call_poster.jpg", "https://www.youtube.com/watch?v=RopRbjny-W0")
 
-movies = [boyz_n_the_hood, new_jack_city, menace_ii_society, hustle_and_flow, juice, set_it_off, friday, poetic_justice, booty_call]
+movies = [new_jack_city, boyz_n_the_hood,  menace_ii_society, hustle_and_flow, juice, set_it_off, friday, poetic_justice, booty_call]
 fresh_tomatoes.open_movies_page(movies)
